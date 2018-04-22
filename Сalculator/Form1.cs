0322262fdb8a8c319d647f428e64b6a6a462df20
Ra1n <degtev98@list.ru>
2018-04-22 17:19:07 +0500

    аддада

--- Сalculator/Form1.cs
+++ Сalculator/Form1.cs
@@ -139,7 +139,7 @@ namespace Сalculator
             ClickPlus(false);//отклчили кнопку
             ClickEnterDays(true);
             ClickEnterMonth(true);
-            //изииииииии
+            //изиииииииидадада
         }
     }
 }
