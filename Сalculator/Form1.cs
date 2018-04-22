061460392cf305d769b998a90edfdd9283ffbc38
Ra1n <degtev98@list.ru>
2018-04-22 18:03:26 +0500

    5555

--- Сalculator/Form1.cs
+++ Сalculator/Form1.cs
@@ -139,7 +139,7 @@ namespace Сalculator
             ClickPlus(false);//отклчили кнопку
             ClickEnterDays(true);
             ClickEnterMonth(true);
-            //изиииииииидадада223444
+            //изиииииииидадада22355555
         }
 
         private void button1_Click(object sender, EventArgs e)
